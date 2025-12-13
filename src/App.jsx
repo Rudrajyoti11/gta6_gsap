62ce79fa996a967ba1f91ff756451df3f1aaaf7f
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2025-12-14 00:19:42 +0530

    Implement Lucia

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './sections/Hero';
 import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
       <FirstVideo/>
       <Jason/>
       <SecondVideo/>
+      <Lucia/>
     </main>
   )
 }
